82a420a4e5f6f9d5b715382e33c83fb6918e9124
Natalie Ng <ngcheokieng@yahoo.com.hk>
2023-12-13 16:09:39 +0000

    update package dependencies url

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "git@github.com:nmdias/FeedKit.git", from: "9.1.2"),
+        .package(url: "https://github.com/nmdias/FeedKit", .upToNextMajor(from: "9.1.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
